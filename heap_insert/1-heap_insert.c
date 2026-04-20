40f8d0bd946ebeaf48168bc6e5b92f4acfea78ae
Tefa95100 <damien.julien.95100@gmail.com>
2026-04-20 02:12:35 +0200

    Add function for exercise 1

---
+++ heap_insert/1-heap_insert.c
@@ -0,0 +1,71 @@
+#include "binary_trees.h"
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return (0);
+	}
+	
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+heap_t *get_parent(heap_t *root, size_t index, size_t bit)
+{
+	if (bit == 1)
+		return (root);
+
+	if (index & bit)
+		return (get_parent(root->right, index, bit >> 1));
+	else
+		return (get_parent(root->left, index, bit >> 1));
+}
+
+void heapify_up(heap_t *node)
+{
+	int tmp;
+
+	while (node->parent && node->n > node->parent->n)
+	{
+		tmp = node->n;
+		node->n = node->parent->n;
+		node->parent->n = tmp;
+		node = node->parent;
+	}
+}
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+	size_t size, bit;
+	heap_t *parent, *new_node;
+
+	if (!root)
+		return (NULL);
+
+	if (*root == NULL)
+		return (*root = binary_tree_node(NULL, value));
+
+	size = binary_tree_size(*root);
+	size++; /* index du nouveau noeud */
+
+	/* trouver le bit de poids fort */
+	bit = 1;
+	while (bit <= size)
+		bit <<= 1;
+	bit >>= 2;
+
+	parent = get_parent(*root, size, bit);
+
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
+
+	if (size & 1)
+		parent->right = new_node;
+	else
+		parent->left = new_node;
+
+	heapify_up(new_node);
+
+	return (new_node);
+}
